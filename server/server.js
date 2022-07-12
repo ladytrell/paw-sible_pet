8a776a400bbdadc6c11411ae7bebff9feb3180d5
Lady Trell <antrellkent@gmail.com>
2022-07-11 20:06:00 -0400

    update graphql apollo-server-express

--- server/server.js
+++ server/server.js
@@ -8,13 +8,20 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-});
 
-server.applyMiddleware({ app });
+let server = null;
+
+async function startServer () {
+  server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
+  await server.start()
+ server.applyMiddleware({ app });
+}
+
+startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -25,11 +32,11 @@ app.use('/images', express.static(path.join(__dirname, '../client/images')));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
+/*
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
+*/
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
